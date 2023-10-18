294407e4187619d7c9b21cba80d3bf291957a342
Roman, Badornyi <roman.badiornyi@gmail.com>
2023-10-18 13:28:32 +0200

    add c# project to the playground.

---
+++ CSharpPlayground/Program.cs
@@ -0,0 +1,12 @@
+﻿var group = new List<User>().GroupBy(x => x.FirstName);
+var groupKeys = group.Select(g => {
+    var (k, v) = g.DeconstructGroup();
+    return (k, v);
+});
+
+record User(string FirstName, string LastName);
+public static class GroupingExtensions
+{
+    public static (T key, IEnumerable<TG> group) DeconstructGroup<T, TG>(this IGrouping<T, TG> group)
+        => (group.Key, group);
+}
--- FSharpPlayground/Program.fs
+++ FSharpPlayground/Program.fs
@@ -2,20 +2,20 @@
 open System;
 
 //wait for keyboard input
-let waitKeyboard() : uint = 
-    match Console.IsOutputRedirected with 
+let waitKeyboard() : uint =
+    match Console.IsOutputRedirected with
         | false -> (uint)(Console.ReadKey().Key)
         | _ -> 0u
 
 //print message
 let message = "hello world"
-printf "%s" message
+printfn "%s" message
 
 //inner let
-let i, j, k = 
+let i, j, k =
     let iInner, jInner, kInner = (1, 2, 3)
     (iInner * 2, jInner * 2, kInner * 2)
-printf "%i_%i_%i\n" i j k
+printfn "%i_%i_%i" i j k
 
 //fibonacci with tail recursion
 let fibonacci n =
@@ -26,32 +26,31 @@ let fibonacci n =
     fib n 0 1
 
 let number = fibonacci 10
-printf "%i\n" number 
+printfn "%i" number
 
 // functions pipe, doing sum of filtered list
 let numbers = [ 1; 2; 3; 4; 5 ]
-let oddNumbersSum = 
+let oddNumbersSum =
     numbers |>
     List.filter(fun x -> x % 2 <> 0) |>
-    List.fold(fun acc  n -> acc + n) 0    
-printf "%i\n" oddNumbersSum
+    List.fold(fun acc  n -> acc + n) 0
 
 // functions composition, doing sum of filtered list
-let sumOfOddNumbers = 
+let sumOfOddNumbers =
     let filterOddNumbers = List.filter(fun x -> x % 2 <> 0)
-    let sumOddNumbers = List.fold(fun acc n -> acc + n) 0 
+    let sumOddNumbers = List.fold(fun acc n -> acc + n) 0
     filterOddNumbers >> sumOddNumbers
 let oddNumbersSumComposition = sumOfOddNumbers numbers
-printf "%i\n" oddNumbersSumComposition
+printfn "%i %i" oddNumbersSum oddNumbersSumComposition
 
 // functions pipe, doing sum of filtered sequence
 let numbersSequence = seq { for i in 1 .. 100 -> i }
-let filteredNumbers threshold = 
+let filteredNumbers threshold =
     numbersSequence |>
     Seq.filter(fun i -> if i > threshold then false else true) |>
     Seq.fold(fun acc n -> acc + n) 0
 let sumFiltered = filteredNumbers 5
-printf "%i\n" sumFiltered
+printfn "%i" sumFiltered
 
 // pattern, recursively decompose list
 let list1 = [ 1; 2; 3; 4 ]
@@ -62,12 +61,12 @@ let rec printList l =
 printList list1
 
 // pattern, recursively decompose list and sum it up
-let rec sumList l = 
+let rec sumList l =
     match l with
         | head :: tail -> head + sumList tail
         | [] -> 0
 let sumSample = sumList list1
-printf "%d" sumSample
+printfn "%d" sumSample
 
 // pattern, decompose list and count number of element that matching certain value
 let countValues list value =
@@ -79,7 +78,87 @@ let countValues list value =
     checkList list 0
 let sampleList = [ for x in -10..10 -> x*x - 4 ]
 let result = countValues sampleList 0
-printfn "\n%d" result
+printfn "%d" result
+
+// using pattern matching match Name field within record and within Union type
+type UserRecord = { Name: string; LastName: string }
+type UserRecordExtended = { Name: string; LastName: string; Address: string }
+
+type User =
+    | Standard of UserRecord
+    | Extended of UserRecordExtended
+
+let IsMatchByName user (name: string) =
+    match user with
+    | { UserRecord.Name = userName; } when userName = name -> true
+    | _ -> false
+
+let IsMatchUserByName user (name: string) =
+    match user with
+    | Standard { Name = userName; } when userName = name -> true
+    | Extended { Name = userName; } when userName = name -> true
+    | _ -> false
+
+let userStandard = { Name = "Parker"; LastName = "Smith" }
+let userExtended = { Name = "Parker"; LastName = "Smith"; Address = "" }
+let user1 = Extended userExtended
+let user2 = Standard userStandard
+
+let isMatched1 = IsMatchByName userStandard "Parker"
+let isMatched2 = IsMatchByName userStandard "Smith"
+let isMatched3 = IsMatchUserByName user1 "Parker"
+let isMatched4 = IsMatchUserByName user2 "Parker"
+printfn "%b %b %b %b" isMatched1 isMatched2 isMatched3 isMatched4
+
+// pattern against union type, when will be executed only if type matches
+type SampleStringUnion =
+    | CaseA of string
+    | CaseB of string
+
+let evaluate value =
+    match value with
+    | CaseA a when a.Length > 10 -> "A's string length is greater than 10"
+    | CaseA _ -> "A's string length is 10 or less"
+    | CaseB b when b.Length > 5 -> "B's string length is greater than 5"
+    | CaseB _ -> "B's string length is 5 or less"
+
+let evaluateOut1= evaluate (CaseA "abcdf")
+let evaluateOut2= evaluate (CaseB "abcdf")
+
+type SampleIntUnion =
+    | A of int
+    | B of int
+
+//this is combined pattern so when will be executed for both of supplied types
+let foo(value: SampleIntUnion) =
+    let test = value
+    match test with
+    | A a
+    | B a when a > 41 -> a // the guard applies to both patterns
+    | _ -> 1
+let evaluate4 = foo (A 9)
+let evaluate5 = foo (B 3)
+
+//active patterns
+
+let (|Even|Odd|) number = if number % 2 = 0 then Even else Odd
+let describeNumber n =
+    match n with
+    | Even -> "even"
+    | Odd -> "odd"
+let evenNumber = describeNumber 4
+let oddNumber = describeNumber 3
+
+let (|Default|) namePattern value =
+    match value with
+    | None -> namePattern
+    | Some valueInternal -> valueInternal
+
+let greet (Default "random citizen" name) =
+    printfn "Hello, %s!" name
+
+greet None
+greet (Some "George")
 
 // end
 do waitKeyboard() |> ignore
